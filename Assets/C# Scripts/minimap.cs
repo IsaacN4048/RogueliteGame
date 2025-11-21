4c2dd6e7f2e49feb388081cf94a51c731dd17fad
hguzman101073 <hguzman008@gmail.com>
2025-11-21 01:37:58 -0500

    I Will work on  a minimap

---
+++ Assets/C# Scripts/minimap.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class minimap : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Minimap script attached and running.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
